5d7d89ff3c9d1ea884f50786ab1cb2145901da3a
PIYUSH KUMAR <142583755+PiyushRepoo@users.noreply.github.com>
2024-03-29 15:09:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const accessKey = "t1cpVcMtLKz51ZIXYHY4e-dtQyuNYJRAnzkTJANZsOU";
+
+const apiKey = "cObNu90NGETWRWHjrcu4XVZQTU7YiLhPLrwLxEASHLAyguhQlXsKcvQc";
+
+const inputBox = document.getElementById("inputBox");
+const mainBox = document.querySelector(".mainBox");
+const main = document.querySelector("main");
+console.log(mainBox);
+const img = document.querySelector("img");
+
+let keyword = "";
+let page = 1;
+// async function searchImages() {
+//   keyword = inputBox.value;
+//   // keyword.replace(/\s/g, "");
+//   console.log(keyword);
+//   const url = `https://api.unsplash.com/search/users?page=${page}&query=${keyword}&client_id=${accessKey}`;
+//   console.log(url);
+//   const res = await fetch(url);
+//   const data = await res.json();
+
+//   const results = data.results;
+
+//   results.map((result) => {
+//     let link = result.photos[0].urls.regular;
+//     console.log(link);
+
+//     let div = document.createElement("div");
+//     div.classList.add("image");
+
+//     let img = document.createElement("img");
+//     img.classList.add("img");
+//     img.src = link;
+
+//     div.append(img);
+//     main.prepend(div);
+//   });
+// }
+
+let per_page = 15;
+let current_page = 1;
+
+async function getImages() {
+  console.log("Run");
+
+  keyword = inputBox.value;
+  const url = `https://api.pexels.com/v1/search?query=${keyword}$page=${current_page}&per_page=${per_page}`;
+
+  let results = await fetch(url, {
+    headers: { Authorization: apiKey },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return data.photos;
+    });
+  // console.log(results);
+
+  results.map((result) => {
+    let link = result.src.original;
+
+    let div = document.createElement("div");
+    div.classList.add("image");
+
+    let img = document.createElement("img");
+    img.classList.add("img");
+    img.src = link;
+
+    div.append(img);
+    mainBox.prepend(div);
+  });
+}
+
+document.addEventListener("keypress", (e) => {
+  // console.log(e.code);
+
+  if (e.code == "KeyB") {
+    inputBox.focus();
+  }
+});
+
+inputBox.addEventListener("keypress", (e) => {
+  // e.preventDefault();
+  // page = 1;
+  // current_page = 1;
+
+  if (e.code == "Enter") {
+    // searchImages();
+    // main.firstElementChild.remove();
+    getImages();
+    inputBox.value = "";
+  }
+});
